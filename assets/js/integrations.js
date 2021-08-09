4f7886b3b00ca72dc45384bfa18a087977a1db7b
nehakumarisaa <nehakumarisaa@gmail.com>
2021-08-09 13:36:19 +0530

    Create integrations.js

---
+++ assets/js/integrations.js
@@ -0,0 +1,2 @@
+<script defer data-domain="reddy2go.com" src="https://plausible.io/js/plausible.js"></script>
+<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="1af648c5-3919-4621-8044-2540e2df76e6";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
